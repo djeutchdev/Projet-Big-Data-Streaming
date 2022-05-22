37682a6b0a113ba5872e5cb2ee9051c5bfa855c6
djeutchdev <djeutchdev@gmail.com>
2022-05-22 22:27:05 +0100

    dernière mise à jour du fichier HelloWorldBigData.scala

--- src/test/scala/HelloWorldBigData.scala
+++ src/test/scala/HelloWorldBigData.scala
@@ -2,6 +2,6 @@ object HelloWorldBigData {
   /*Premier programme en scala */
 def main (args: Array[String]): Unit ={
   println("Mon premier code en Scala")
-  println("Ici c'est un test de vie ok")
+  println("Ici c'est un test de bon fonctionnement de mon premier code scala")
 }
 }
